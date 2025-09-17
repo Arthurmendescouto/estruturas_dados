673eee310925e510675978292306414f2c32ddb7
Arthur <arthurmendescouto16@gmail.com>
2025-09-17 00:42:25 -0300

    exercício 3 da lista 2

---
+++ ListaDeExercicios2/src/Ex3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor de N: ");
+        int n=sc.nextInt();
+
+        int[] numeros=new int[n];
+
+        for(int i=0;i<n;i++){
+            numeros[i]=n-i;
+        }
+
+        System.out.println("Array em ordem inversa:");
+        for (int i = 0; i <n ; i++) {
+            System.out.println(numeros[i]+" ");
+        }
+        sc.close();
+    }
+}
