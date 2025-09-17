f9163751e8f12a15533228925332d15255d5c77d
Arthur <arthurmendescouto16@gmail.com>
2025-09-17 00:42:58 -0300

    código completo do Ex4 da lista 2

---
+++ ListaDeExercicios2/src/Ex4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Quantos números você quer inserir?");
+        int n=sc.nextInt();
+
+        int[] numeros=new int[n];
+
+        for (int i = 0; i <n ; i++) {
+            System.out.println("Digite o número "+ (i+1)+": ");
+            numeros[i]= sc.nextInt();
+        }
+
+        int maior=numeros[0];
+        int menor=numeros[0];
+        for (int i = 1; i <n ; i++) {
+            if (numeros[i]>maior){
+                maior=numeros[i];
+            }
+            if(numeros[i]<menor){
+                menor=numeros[i];
+            }
+
+        }
+        System.out.println("\nMaior número: " + maior);
+        System.out.println("Menor número: " + menor);
+
+        sc.close();
+    }
+}
