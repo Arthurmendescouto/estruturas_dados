fef1068f6aed6da832b75329a984a6ffc1337c6a
Arthur <arthurmendescouto16@gmail.com>
2025-09-18 01:45:12 -0300

    ArrayList finalizado

--- List/ArrayList.java
+++ List/ArrayList.java
@@ -1,6 +1,6 @@
 package List;
 
-public class ArrayList implements Listable{
+public class ArrayList implements Listable {
     private Object[] data;
     private int head;
     private int tail;
@@ -9,97 +9,177 @@ public class ArrayList implements Listable{
     public ArrayList() {
         this(10);
     }
-    public ArrayList(int length){
-        data=new Object[length];
-        head=0;
-        tail=-1;
-        numberElements=0;
+
+    public ArrayList(int length) {
+        data = new Object[length];
+        head = 0;
+        tail = -1;
+        numberElements = 0;
     }
 
     @Override
-    public void insert(Object index,int logicIndex){
-      if(!isFull()){
-          if (logicIndex>=0 && logicIndex <=numberElements{
-              int physicalIndex=map(logicIndex);
-
-              
-
-          }
-      }
-    }
-    @Override
-    public void append(Object item){
-        if(!isFull()){
+    public void insert(int index, Object item) {
+        if (isFull()) {
+            System.out.println("List is full!");
+            return;
+        }
+        if (index < 0 || index > numberElements) {
+            System.err.println("Invalided Index");
+            return;
+        }
+        if (index < numberElements / 2) {
+            int physicalIndex = map(index);
+            int aux = head;
+            for (int i = 0; i < index; i++) {
+                data[prior(aux)] = data[aux];
+                aux = next(aux);
+            }
+            data[physicalIndex] = item;
+            head = prior(head);
+        } else {
+            int physicalIndex = map(index);
+            int aux=tail;
+            for (int i = 0; i <numberElements-index; i++) {
+                data[next(aux)]=data[aux];
+                aux=prior(aux);
+            }
+            data[physicalIndex]=item;
             tail=next(tail);
-            data[tail]=item;
+        }
+        numberElements++;
+    }
+
+    @Override
+    public Object delete(int index){
+        if (isEmpty()) {
+            System.out.println("List is empty!");
+            return null;
+
+        }
+        if (index < 0 || index >= numberElements) {
+            System.err.println("Invalided Index");
+            return null;
+        }
+        int physicalIndex=map(index);
+        Object removedElement=data[physicalIndex];
+
+        if (index < numberElements / 2){
+            int aux=physicalIndex;
+            for (int i = 0; i <index ; i++) {
+            data[aux]=data[prior(aux)];
+            aux=prior(aux);
+            }
+            head=next(head);
+        }else{
+            int aux=physicalIndex;
+            for (int i = 0; i <numberElements-index -1; i++) {
+                data[aux]=data[next(aux)];
+                aux=next(aux);
+            }
+            tail=prior(tail);
+        }
+        numberElements--;
+        return removedElement;
+    }
+
+    @Override
+    public void append(Object item) {
+        if (!isFull()) {
+            tail = next(tail);
+            data[tail] = item;
             numberElements++;
-        }else {
+        } else {
             System.err.println("A lista está cheia!");
         }
     }
+
     @Override
-    public boolean isEmpty(){
-        return numberElements==0;
+    public void update(int index, Object item) {
+        if(isEmpty()){
+            System.out.println("List is empty!");
+            return;
+        }
+        if (index < 0 || index >= numberElements) {
+            System.err.println("Invalid Index");
+            return;
+        }
+        int physicalIndex=map(index);
+        data[physicalIndex]=item;
     }
+
     @Override
-    public boolean isFull(){
-        return numberElements== data.length;
+    public boolean isEmpty() {
+        return numberElements == 0;
     }
+
     @Override
-    public String print(){
-        String result="";
-        int aux =head;
-        for (int i=0;i<numberElements;i++){
-            result+=data[aux];
-            aux=next(aux);
-            if (i!=numberElements-1){
-                result+=", ";
+    public boolean isFull() {
+        return numberElements == data.length;
+    }
+
+    @Override
+    public String print() {
+        String result = "";
+        int aux = head;
+        for (int i = 0; i < numberElements; i++) {
+            result += data[aux];
+            aux = next(aux);
+            if (i != numberElements - 1) {
+                result += ", ";
             }
         }
         return result;
     }
-    @Override
-    public void clear(){
-        head=0;
-        tail=-1;
-        numberElements=0;
-    }
-    @Override
-    public Object select(int logicIndex){
-    Object itemAux=null;
-    if(!isEmpty()){
-        if((logicIndex>=0) && (logicIndex<numberElements)){
-          int physicalIndex=map(logicIndex);
-            itemAux=data[physicalIndex];
-        }else{
-            System.err.println("Indice Invalido!");
-        }
-    }else {
-        System.err.println("Lista Vazia!");
-    }
-    return itemAux;
 
+    @Override
+    public void clear() {
+        head = 0;
+        tail = -1;
+        numberElements = 0;
     }
 
     @Override
-    public Object[] selectAll(){
-        Object[] dataAux=null;
-        if(!isEmpty()){
-         dataAux=new Object[numberElements];
-         int pointerAux=head;
-            for (int i = 0; i < data.length ; i++) {
-                dataAux[i]=pointerAux;
-                pointerAux=next(pointerAux);
+    public Object select(int logicIndex) {
+        Object itemAux = null;
+        if (!isEmpty()) {
+            if ((logicIndex >= 0) && (logicIndex < numberElements)) {
+                int physicalIndex = map(logicIndex);
+                itemAux = data[physicalIndex];
+            } else {
+                System.err.println("Indice Invalido!");
             }
-        }else{
+        } else {
+            System.err.println("Lista Vazia!");
+        }
+        return itemAux;
+
+    }
+
+    @Override
+    public Object[] selectAll() {
+        Object[] dataAux = null;
+        if (!isEmpty()) {
+            dataAux = new Object[numberElements];
+            int pointerAux = head;
+            for (int i = 0; i < numberElements; i++) {
+                dataAux[i] = data[pointerAux];
+                pointerAux = next(pointerAux);
+            }
+        } else {
             System.err.println("Lista está vazia!");
         }
         return dataAux;
     }
-    private int next(int index){
-        return (data.length+index+1)% data.length;
+
+    private int next(int index) {
+        return (data.length + index + 1) % data.length;
     }
-    private int map(int index){
-        return (index+head)% data.length;
+
+    private int prior(int index) {
+        return (data.length + index - 1) % data.length;
+    }
+
+    private int map(int index) {
+        return (index + head) % data.length;
     }
 }
--- List/Listable.java
+++ List/Listable.java
@@ -1,11 +1,11 @@
 package List;
 
 public interface Listable {
-    void insert(Object data,int LogicIndex);
+    void insert(int LogicIndex, Object data);
     void append(Object data);
     Object select(int LogicIndex);
     Object[] selectAll();
-    void update(Object data, int LogicalIndex);
+    void update( int index, Object item);
     Object delete(int LogicalIndex);
     void clear();
     boolean isEmpty();
