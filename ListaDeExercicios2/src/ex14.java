86c1f7eb8ada2144af82603b9c5d4b3aac638ded
Arthur <arthurmendescouto16@gmail.com>
2025-09-18 10:03:40 -0300

    ex 14 da lista 2

---
+++ ListaDeExercicios2/src/ex14.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ex14 {
+    public static void main(String[] args) {
+        int[] numeros={10,15,8,40,22};
+        Arrays.sort(numeros);
+        System.out.println("Array em ordem crescente: ");
+        for (int num: numeros){
+            System.out.println(num+" ");
+        }
+    }
+}
