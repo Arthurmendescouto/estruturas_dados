e34cf634bae1425ceae7518989068c99ab2af108
Arthur <arthurmendescouto16@gmail.com>
2025-09-20 14:31:01 -0300

    ex26 da lista 2

--- ListaDeExercicios2/src/Ex26.java
+++ ListaDeExercicios2/src/Ex26.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class Ex26 {
+
+    public static boolean isPrimo(int num){
+        if(num<2){
+            return false;
+        }
+        for (int i = 2; i <Math.sqrt(num) ; i++) {
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Digite quantos números primos você quer gerar: ");
+        int n=sc.nextInt();
+
+        int[] primos=new int[n];
+        int count=0;
+        int numero=2;
+
+        while (count<n){
+            if(isPrimo(numero)){
+                primos[count]=numero;
+                count++;
+            }
+            numero++;
+        }
+
+        System.out.println("Os "+n+"primeiros números primos são:");
+        for (int i = 0; i <n ; i++) {
+            System.out.println(primos[i]+" ");
+        }
+
+    }
 }
+
