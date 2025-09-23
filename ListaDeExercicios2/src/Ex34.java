f7471a8d2fb8f0ff2cf19a01dd3410966fc70e33
Arthur <arthurmendescouto16@gmail.com>
2025-09-23 19:38:30 -0300

    ex34 lista 2

--- ListaDeExercicios2/src/Ex34.java
+++ ListaDeExercicios2/src/Ex34.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+
 public class Ex34 {
+    public static void main(String[] args) {
+        int[] numeros={10,20,30,40};
+        System.out.println("Antes: "+ Arrays.toString(numeros));
+
+        int novoElemento=99;
+        int posicao=2;
+
+        int[] novoArray=new int[numeros.length+1];
+
+        for (int i = 0; i <posicao ; i++) {
+            novoArray[i]=numeros[i];
+        }
+        novoArray[posicao]=novoElemento;
+
+        for (int i = posicao; i < numeros.length ; i++) {
+            novoArray[i+1]=numeros[i];
+        }
+
+        numeros=novoArray;
+
+        System.out.println("Depois: "+Arrays.toString(numeros));
+    }
 }
