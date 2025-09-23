f9b225a820a1f0293fc265f23281a2b8fbf22a39
Arthur <arthurmendescouto16@gmail.com>
2025-09-23 01:00:37 -0300

    ex33 lista 2

---
+++ Heap/src/ArvoreBinariaHeapMaxima.java
@@ -0,0 +1,20 @@
+public class ArvoreBinariaHeapMaxima {
+    private Object[] data;
+    int tail;
+
+    public ArvoreBinariaHeapMaxima(Object[] data) {
+        this.data = data;
+        this.tail = -1;
+    }
+
+    public ArvoreBinariaHeapMaxima(int length) {
+        data=new Object[length];
+        tail=-1;
+    }
+
+    public void inserir(Object data){
+        if (isFull()) {
+            System.err.println("Heap Cheia!");
+        }
+    }
+}
--- ListaDeExercicios2/src/Ex33.java
+++ ListaDeExercicios2/src/Ex33.java
@@ -1,3 +1,21 @@
-public class Ex33 {
+import java.util.Arrays;
 
+public class Ex33 {
+    public static void main(String[] args) {
+        int[] numeros={10,20,30,40};
+        System.out.println("Antes: "+ Arrays.toString(numeros));
+
+        int novoElemento=50;
+        int[] novoArray=new int[numeros.length+1];
+
+        for (int i = 0; i <numeros.length ; i++) {
+         novoArray[i]=numeros[i];
+        }
+
+        novoArray[numeros.length]=novoElemento;
+
+        numeros=novoArray;
+
+        System.out.println("Depois: "+Arrays.toString(numeros));
+    }
 }
