4867368944e9d98384a6bf643b070b343870000d
Arthur <arthurmendescouto16@gmail.com>
2025-09-23 21:04:26 -0300

    ex2 lista1

--- ListaDeExercicios/src/Ex2.java
+++ ListaDeExercicios/src/Ex2.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Ex2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double[] numeros=new double[10];
+
+        System.out.println("Digite 10 números reais:");
+        for (int i = 0; i < numeros.length ; i++) {
+            numeros[i]=sc.nextDouble();
+        }
+        System.out.println("\nNúmeros na ordem inversa:");
+        for (int i = numeros.length-1; i >=0 ; i--) {
+            System.out.println(numeros[i]+" ");
+        }
+        sc.close();
+    }
 }
