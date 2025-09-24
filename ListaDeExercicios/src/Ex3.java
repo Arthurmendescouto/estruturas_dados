328fe227e453c58d9dd1f7662096133c0864c559
Arthur <arthurmendescouto16@gmail.com>
2025-09-23 22:54:21 -0300

    ex3 lista 1

---
+++ ListaDeExercicios/src/Ex3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        double[] notas=new double[4];
+        double soma=0;
+
+        System.out.println("Digite 4 notas:");
+        for (int i = 0; i <notas.length ; i++) {
+         notas[i]= sc.nextDouble();
+         soma+=notas[i];
+        }
+        double media=soma/ notas.length;
+
+        System.out.println("\nNotas digitadas:");
+        for (double nota:notas) {
+            System.out.println(nota+" ");
+        }
+        System.out.println("\nMédia"+media);
+        sc.close();
+    }
+}
