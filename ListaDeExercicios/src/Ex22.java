8f54001cd41da74cb1baa0d3c137bc58789445a6
Arthur <arthurmendescouto16@gmail.com>
2025-09-27 21:59:16 -0300

    ex 22 lista 1

--- ListaDeExercicios/src/Ex22.java
+++ ListaDeExercicios/src/Ex22.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class Ex22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] defeitos = new int[4];
+        int total = 0;
+
+        System.out.println("Digite o número de identificação e o código do defeito (0 encerra):");
+        while (true){
+            int id=sc.nextInt();
+            if(id==0) break;
+            int cod=sc.nextInt();
+            if(cod>=1 && cod <=4){
+                defeitos[cod-1]++;
+                total++;
+            }
+        }
+        System.out.println("\nQuantidade de mouses: " + total);
+        System.out.println("\nSituação\t\t\tQuantidade\tPercentual");
+
+        String[] nomes={
+                "1- necessita da esfera",
+                "2- necessita de limpeza",
+                "3- necessita troca do cabo ou conector",
+                "4- quebrado ou inutilizado"
+        };
+        for (int i = 0; i <4 ; i++) {
+            double perc=total>0? defeitos[i]*100.0/total : 0;
+            System.out.printf("%-35s %-10d %.0f%%\n",nomes[i],defeitos[i],perc);
+        }
+sc.close();
+    }
 }
