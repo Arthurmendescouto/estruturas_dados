21e56ec9729e9bde99ea167e9fe11a2792138222
Arthur <arthurmendescouto16@gmail.com>
2025-09-27 23:18:54 -0300

    ex24 lista 1

--- ListaDeExercicios/src/Ex24.java
+++ ListaDeExercicios/src/Ex24.java
@@ -1,2 +1,20 @@
+import java.util.Random;
+
 public class Ex24 {
+    public static void main(String[] args) {
+        int lancamentos=100;
+        int[] resultados=new int[lancamentos];
+        int[] contadores=new int[6];
+
+        Random random=new Random();
+        for (int i = 0; i <lancamentos ; i++) {
+            int valor=random.nextInt(6)+1;
+            resultados[i]=valor;
+            contadores[valor-1]++;
+        }
+        System.out.println("Resultado dos "+lancamentos+" lançamentos:");
+        for (int i = 0; i <contadores.length ; i++) {
+            System.out.println("Número "+(i+1)+": "+contadores[i]+" vezes");
+        }
+    }
 }
